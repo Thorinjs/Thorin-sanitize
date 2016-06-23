cb27d696dc3621479a05a62c06c297031dcc4313
snupa <bunta.adrian@gmail.com>
2016-06-23 11:32:00 +0300

    string sanitizer now accepts numbers/booleans and toStrings() them.

--- items/string.js
+++ items/string.js
@@ -4,28 +4,41 @@
  */
 module.exports = (IFace) => {
   return class SanitizeString extends IFace {
-    static code() { return "STRING" };
-    static aliases() { return ["TEXT", "LONGTEXT", "VARCHAR"] }
-    static publicName() { return "String"; }
+    static code() {
+      return "STRING"
+    };
+
+    static aliases() {
+      return ["TEXT", "LONGTEXT", "VARCHAR"]
+    }
+
+    static publicName() {
+      return "String";
+    }
 
     /* Validate the string input
-    * OPTIONS:
-    *   - min = the minimum length the string must have
-    *   - max = the maximum length the string must have.
-    * */
+     * OPTIONS:
+     *   - min = the minimum length the string must have
+     *   - max = the maximum length the string must have.
+     * */
     validate(d, opt) {
-      if(typeof d !== 'string' || !d) return false;
+      if (typeof d === 'number') {
+        d = d.toString();
+      } else if (typeof d === 'boolean') {
+        d = d.toString();
+      }
+      if (typeof d !== 'string' || !d) return false;
       d = d.trim();
-      if(d === '') return false;
-      if(opt.min > 0 && d.length < opt.min) {
+      if (d === '') return false;
+      if (opt.min > 0 && d.length < opt.min) {
         return false;
       }
-      if(opt.max > 0 && d.length > opt.max) {
+      if (opt.max > 0 && d.length > opt.max) {
         return false;
       }
       d = d.replace(/'/g, "\x27");
       d = d.replace(/"/g, "\x22");
-      if(opt.html !== true) {
+      if (opt.html !== true) {
         d = d.replace(/</g, "&lt;");
         d = d.replace(/>/g, "&gt;");
       }
