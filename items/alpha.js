c5bcddcc95d9466c7f037f892c9ef46bcddba259
snupa <bunta.adrian@gmail.com>
2016-08-24 13:58:44 +0300

    added min/max for alpha sanitization

--- items/alpha.js
+++ items/alpha.js
@@ -6,8 +6,13 @@ const validator = require('validator');
 module.exports = (IFace) => {
 
   return class SanitizeAlpha extends IFace {
-    static code() { return "ALPHA" };
-    static publicName() { return "Alpha"; }
+    static code() {
+      return "ALPHA"
+    };
+
+    static publicName() {
+      return "Alpha";
+    }
 
     /*
      * Verifies if the given input is alpha-string
@@ -16,22 +21,25 @@ module.exports = (IFace) => {
      *  - dash=false -> should we allow dashes in the alpha?
      *  - underscore=false -> should we allow underscore in the alpha?
      *  - numeric=false -> transforms into AlphaNumeric
+     *  - min/max -> max string length
      * */
     validate(d, opt) {
-      if(typeof d !== 'string') return false;
+      if (typeof d !== 'string') return false;
       d = d.trim();
       let checker = d;
-      if(opt.dot === true) {
+      if (opt.dot === true) {
         checker = checker.replace(/\./g, '');
       }
-      if(opt.dash === true) {
-        checker = checker.replace(/\-/g,'');
+      if (opt.dash === true) {
+        checker = checker.replace(/\-/g, '');
       }
-      if(opt.underscore === true) {
-        checker = checker.replace(/_/g,'');
+      if (opt.underscore === true) {
+        checker = checker.replace(/_/g, '');
       }
       let validateFn = (opt.numeric === true ? 'isAlphanumeric' : 'isAlpha');
-      if(!validator[validateFn](checker)) return false;
+      if (!validator[validateFn](checker)) return false;
+      if (typeof opt.max === 'number' && d.length > opt.max) return false;
+      if (typeof opt.min === 'number' && d.length < opt.min) return false;
       return {
         value: d
       };
