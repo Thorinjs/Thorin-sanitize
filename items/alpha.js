7d3c98caf19ab3ccc244d0417cee5a8d3b4a04cc
snupa <bunta.adrian@gmail.com>
2017-02-04 11:22:51 +0200

    added slash= option for alpha sanitization

--- items/alpha.js
+++ items/alpha.js
@@ -22,6 +22,7 @@ module.exports = (IFace) => {
      *  - dash=false -> should we allow dashes in the alpha?
      *  - underscore=false -> should we allow underscore in the alpha?
      *  - numeric=false -> transforms into AlphaNumeric
+     *  - slash=false -> if set to true, will allow single-forward slash
      *  - min/max -> max string length
      * */
     validate(d, opt) {
@@ -30,6 +31,10 @@ module.exports = (IFace) => {
       let checker = d;
       if (opt.dot === true) {
         checker = checker.replace(/\./g, '');
+      }
+      if (opt.slash === true) {
+        checker = checker.replace(/\//g, '');
+        checker = checker.replace(/\\/g, '');
       }
       if (opt.colon === true) {
         checker = checker.replace(/:/g, '');
@@ -44,6 +49,10 @@ module.exports = (IFace) => {
       if (!validator[validateFn](checker)) return false;
       if (typeof opt.max === 'number' && d.length > opt.max) return false;
       if (typeof opt.min === 'number' && d.length < opt.min) return false;
+      if (opt.slash === true) {
+        d = d.replace(/\/+/g, '/');
+        d = d.replace(/\\+/gm, '/');
+      }
       return {
         value: d
       };
