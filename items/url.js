ee34c966e3fd92e79b1c5fdc6821f915fc7dedf2
snupa <adrian@unloq.io>
2018-01-03 12:48:00 +0200

    added opt.wildcard=true for URL sanitize, of type: *.example.com

--- items/url.js
+++ items/url.js
@@ -19,18 +19,39 @@ module.exports = (IFace) => {
      *   public=true -> does the URL HAVE to be public?
      *   protocol="http,https" -> the protocols we allow.
      *   parse=false -> if set to true, we will return the actual href in the result, not the parsed url
+     *   wildcard=false -> if set to true, validate *.john.com domains
      * */
     validate(d, opt) {
       if (typeof d !== 'string' || !d) return false;
       if (!opt.protocol) opt.protocol = "http,https";
       let obj,
-        protocols = (opt.protocol instanceof Array ? opt.protocol : opt.protocol.replace(/ /g, '').split(','));
+        protocols = (opt.protocol instanceof Array ? opt.protocol : opt.protocol.replace(/ /g, '').split(',')),
+        hasWildcard = (d.indexOf('*') !== -1);
+      if (hasWildcard) {
+        if (opt.wildcard !== true) return false;
+        if (d.indexOf('*') !== d.lastIndexOf('*')) return false;  // no more than 1 star
+        try {
+          let _check = d.split('://')[1].replace(/ /g, '').split('.');
+          if (_check.length > 40) return false;
+          if (_check[_check.length - 1] === '*') return false;
+          if (_check[_check.length - 2] === '*') return false;
+          if (_check[0] !== '*') return false;  // only allow *.subdomain.com
+        } catch (e) {
+          return false;
+        }
+        d = d.replace(/\*/g, '__wildcard__');
+      }
       try {
         obj = url.parse(d);
       } catch (e) {
         return false;
       }
       if (!obj.protocol) return false;
+      if (hasWildcard) {
+        obj.wildcard = true;
+        if (obj.host) obj.host = obj.host.split('__wildcard__').join('*');
+        if (obj.hostname) obj.hostname = obj.hostname.split('__wildcard__').join('*');
+      }
       let hasProto = false;
       for (let i = 0; i < protocols.length; i++) {
         if (obj.protocol.toLowerCase() === protocols[i].toLowerCase() + ':') {
