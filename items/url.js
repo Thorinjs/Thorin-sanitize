bd6320b7baa215c77b115a3708af27eef50b4271
snupa <adrian@unloq.io>
2018-02-07 17:06:38 +0200

    version bump

--- items/url.js
+++ items/url.js
@@ -19,27 +19,17 @@ module.exports = (IFace) => {
      *   public=true -> does the URL HAVE to be public?
      *   protocol="http,https" -> the protocols we allow.
      *   parse=false -> if set to true, we will return the actual href in the result, not the parsed url
-     *   wildcard=false -> if set to true, validate *.john.com domains
+     *   hash=false -> if set to true, do not strip the hash from the url
      * */
     validate(d, opt) {
       if (typeof d !== 'string' || !d) return false;
-      if (!opt.protocol) opt.protocol = "http,https";
+      if (typeof opt.protocol === 'undefined') opt.protocol = "http,https";
       let obj,
-        protocols = (opt.protocol instanceof Array ? opt.protocol : opt.protocol.replace(/ /g, '').split(',')),
-        hasWildcard = (d.indexOf('*') !== -1);
-      if (hasWildcard) {
-        if (opt.wildcard !== true) return false;
-        if (d.indexOf('*') !== d.lastIndexOf('*')) return false;  // no more than 1 star
-        try {
-          let _check = d.split('://')[1].replace(/ /g, '').split('.');
-          if (_check.length > 40) return false;
-          if (_check[_check.length - 1] === '*') return false;
-          if (_check[_check.length - 2] === '*') return false;
-          if (_check[0] !== '*') return false;  // only allow *.subdomain.com
-        } catch (e) {
-          return false;
-        }
-        d = d.replace(/\*/g, '__wildcard__');
+        protocols;
+      if (opt.protocol instanceof Array) {
+        protocols = opt.protocol;
+      } else if (typeof opt.protocol === 'string') {
+        protocols = opt.protocol.replace(/ /g, '').split(',');
       }
       try {
         obj = url.parse(d);
@@ -47,19 +37,16 @@ module.exports = (IFace) => {
         return false;
       }
       if (!obj.protocol) return false;
-      if (hasWildcard) {
-        obj.wildcard = true;
-        if (obj.host) obj.host = obj.host.split('__wildcard__').join('*');
-        if (obj.hostname) obj.hostname = obj.hostname.split('__wildcard__').join('*');
-      }
-      let hasProto = false;
-      for (let i = 0; i < protocols.length; i++) {
-        if (obj.protocol.toLowerCase() === protocols[i].toLowerCase() + ':') {
-          hasProto = true;
-          break;
+      if (protocols instanceof Array) {
+        let hasProto = false;
+        for (let i = 0; i < protocols.length; i++) {
+          if (obj.protocol.toLowerCase() === protocols[i].toLowerCase() + ':') {
+            hasProto = true;
+            break;
+          }
         }
+        if (!hasProto) return false;
       }
-      if (!hasProto) return false;
       if (opt.public === true) {
         let tmpHost = obj.host.split('.'),
           isLocal = false;
@@ -82,12 +69,12 @@ module.exports = (IFace) => {
         obj.href = obj.href.split('#')[0];
       }
       if (obj.port == null) {
-        obj.port = (obj.protocol === 'http:' ? 80 : 443);
+        obj.port = (obj.protocol === 'http:' ? 80 : obj.protocol === 'https:' ? 443 : null);
       } else {
         obj.port = parseInt(obj.port, 10);
       }
-      // remove double quotes of the href and/or path
-      if (obj.path.indexOf('//') !== -1) {
+      // remove double quotes of the href and/or path ONLY for http(s) protocols
+      if (obj.path && obj.path.indexOf('//') !== -1 && (obj.protocol === 'http:' || obj.protocol === 'https:')) {
         const regex = /\/\/+/g;
         obj.path = obj.path.replace(regex, '/');
         obj.pathname = obj.pathname.replace(regex, '/');
