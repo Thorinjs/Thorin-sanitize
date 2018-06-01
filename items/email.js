ccc597464ed6d7d59d3a0c52abafc4696a5bf095
snupa <adrian@unloq.io>
2018-06-01 08:28:08 +0300

    added empty:true on emails

--- items/email.js
+++ items/email.js
@@ -6,21 +6,33 @@ const validator = require('validator');
 module.exports = (IFace) => {
 
   return class SanitizeEmail extends IFace {
-    static code() { return "EMAIL" };
-    static publicName() { return "Email"; }
+    static code() {
+      return "EMAIL"
+    };
+
+    static publicName() {
+      return "Email";
+    }
 
     /*
     * Verifies if the given input is an e-mail.
     * OPTIONS:
     *   tld: true -> if we need to verify the tld
+    *   empty -> if set to true, allow empty string
     * */
     validate(d, opt) {
-      if(typeof d !== 'string') return false;
+      if (typeof d !== 'string') return false;
       d = d.trim();
+      if (d === '') {
+        if (opt.empty === true) return {
+          value: ''
+        };
+        return false;
+      }
       let v = validator.isEmail(d, {
         require_tld: (opt.tld !== false)
       });
-      if(!v) return false;
+      if (!v) return false;
       d = validator.normalizeEmail(d, {
         remove_dots: false,
         remove_extension: false
